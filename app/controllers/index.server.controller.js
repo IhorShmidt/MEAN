fa4f6a114a2c0e05fe36a04097c3b9140268579f
IhorShmidt <ihshmidt@gmail.com>
2016-04-05 22:36:24 +0300

    Adding some other stuff

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,15 +1,4 @@
 exports.render = function (req, res) {
    res.send('Hello world');
 };
-exports.render = function (req, res) {
-   res.send('Hello world');
-};
-exports.render = function (req, res) {
-   res.send('Hello world');
-};
-exports.render = function (req, res) {
-   res.send('Hello world');
-};
-exports.render = function (req, res) {
-   res.send('Hello world');
-};
+
