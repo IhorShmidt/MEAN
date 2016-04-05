35692963ee83e1ccb6a1455a127e918922e54c3a
IhorShmidt <ihshmidt@gmail.com>
2016-04-06 00:41:48 +0300

    Adding some view with ejs module

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,11 +1,11 @@
 exports.render = function(req, res) {
 
-if (req.session.lastVisit) {
-console.log(req.session.lastVisit);
-}
-req.session.lastVisit = new Date();
+    if (req.session.lastVisit) {
+        console.log(req.session.lastVisit);
+    }
+    req.session.lastVisit = new Date();
 
-res.render('index', {
- title: 'Hello World'
-});
+    res.render('index', {
+        title: 'Hello World'
+    });
 };
