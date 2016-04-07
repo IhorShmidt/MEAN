665f7788c836943fbd4f6232ab453190530ef204
IhorShmidt <ihshmidt@gmail.com>
2016-04-07 23:05:23 +0300

    working with mongoose

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,11 +1,18 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Create a new 'render' controller method
 exports.render = function(req, res) {
+	// If the session's 'lastVisit' property is set, print it out in the console
+	if (req.session.lastVisit) {
+		console.log(req.session.lastVisit);
+	}
 
-    if (req.session.lastVisit) {
-        console.log(req.session.lastVisit);
-    }
-    req.session.lastVisit = new Date();
+	// Set the session's 'lastVisit' property
+	req.session.lastVisit = new Date();
 
-    res.render('index', {
-        title: 'Hello World'
-    });
+	// Use the 'response' object to render the 'index' view with a 'title' property
+	res.render('index', {
+		title: 'Hello World'
+	});
 };
--- config/express.js
+++ config/express.js
@@ -5,30 +5,35 @@ var config = require('./config'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
     session = require('express-session');
+
 module.exports = function() {
-    var app = express();
-    if (process.env.NODE_ENV === 'development') {
-        app.use(morgan('dev'));
-    } else if (process.env.NODE_ENV === 'production') {
-        app.use(compress());
-    }
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-    app.use(bodyParser.json());
-    app.use(methodOverride());
+  var app = express();
 
-    app.use(session({
-        saveUninitialized: true,
-        resave: true,
-        secret: config.sessionSecret
-    }));
+  if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  } else if (process.env.NODE_ENV === 'production') {
+    app.use(compress());
+  }
 
-    app.set('views', './app/views');
-    app.set('view engine', 'ejs');
-    require('../app/routes/index.server.routes.js')(app);
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
 
-    app.use(express.static('./public'));
+  app.use(session({
+    saveUninitialized: true,
+    resave: true,
+    secret: config.sessionSecret
+  }));
 
-    return app;
+  app.set('views', './app/views');
+  app.set('view engine', 'ejs');
+
+  require('../app/routes/index.server.routes.js')(app);
+  require('../app/routes/users.server.routes.js')(app);
+
+  app.use(express.static('./public'));
+
+  return app;
 };
